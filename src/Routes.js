2a07c4151575c81423650aaf86b90af3d1832cb2
Milica Medic <mi.medic@levi9.com>
2021-06-04 11:01:53 +0200

    Authentication with Amplify

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
         <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
         <Route component={NotFound}/>
     </Switch>
   );
